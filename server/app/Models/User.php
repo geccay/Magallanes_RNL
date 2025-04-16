8cdd77b9f883f2c7f33f9b7d588408987d5aa5ce
geccay <magallanesangelica97@gmail.com>
2025-04-16 22:22:27 +0800

    AddUser wala pa natapos

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasApiTokens, Notifiable;
 
     protected $table = 'tbl_users';
     protected $primaryKey = 'user_id';
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'last_name',
         'suffix_name',
         'age',
+        'birth_date',
         'gender_id',
         'address',
         'contact_number',
