29c590d1b1f0f66553c5dc4ef9d787022dc75c89
geccay <magallanesangelica97@gmail.com>
2025-05-25 10:44:56 +0800

    RNL done

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -40,74 +40,66 @@ const Navbar = () => {
 
   const handleUserFullName = () => {
     const user = localStorage.getItem("user");
+    const parsedUser = user ? JSON.parse(user) : null;
 
-    if (!user) return "";
+    let fullName = "";
 
-    try {
-      const parsedUser = JSON.parse(user);
-
-      if (!parsedUser || !parsedUser.first_name || !parsedUser.last_name) {
-        return "";
-      }
-
-      const firstName = parsedUser.first_name;
-      const lastName = parsedUser.last_name;
-      const middleInitial = parsedUser.middle_name?.[0];
-
-      return middleInitial
-        ? `${lastName}, ${firstName} ${middleInitial}.`
-        : `${lastName}, ${firstName}`;
-    } catch (error) {
-      console.error("Failed to parse user from localStorage:", error);
-      return "";
+    if (parsedUser.middle_name) {
+      fullName = `${parsedUser.last_name}, ${parsedUser.first_name} ${parsedUser.middle_name[0]}.`;
+    } else {
+      fullName = `${parsedUser.last_name}, ${parsedUser.first_name}`;
     }
+
+    return fullName;
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        RnL Demo
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav mr-auto">
-          {menuItems.map((menuItem, index) => (
-            <li className="nav-item" key={index}>
-              <Link className="nav-link" to={menuItem.route}>
-                {menuItem.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <span className="navbar-text mr-3">{handleUserFullName()}</span>
-
-        <button
-          type="submit"
-          className="btn btn-danger"
-          onClick={handleLogout}
-          disabled={loadingLogout}
-        >
-          {loadingLogout ? (
-            <>
-              <SpinnerSmall /> Logging Out...
-            </>
-          ) : (
-            "Logout"
-          )}
-        </button>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            RnL Demo
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              {menuItems.map((menuItem, index) => (
+                <li className="nav-item" key={index}>
+                  <Link className="nav-link" to={menuItem.route}>
+                    {menuItem.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            {handleUserFullName()}
+          </div>
+          <button
+            type="submit"
+            className="btn btn-danger"
+            onClick={handleLogout}
+            disabled={loadingLogout}
+          >
+            {loadingLogout ? (
+              <>
+                <SpinnerSmall /> Logging Out...
+              </>
+            ) : (
+              "Logout"
+            )}
+          </button>
+        </div>
+      </nav>
+    </>
   );
 };
 
